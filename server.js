4c5fa6e131ce59546d31ff09e5b9bdba25016bfd
Heather Hern <heathernewlin1@gmail.com>
2020-11-25 00:09:35 -0500

    Working on adding heroku

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.listen(PORT, function() {
+    console.log("App now listening at localhost:" + PORT);
+});
